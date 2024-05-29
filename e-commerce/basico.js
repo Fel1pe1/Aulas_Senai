7f42c64f25e3055bc7afe06d5a231f17499931a8
Felipe Meneghel <felipemeneghel241@gmail.com>
2024-05-29 16:09:46 -0300

    subindo e-commerse

---
+++ e-commerce/basico.js
@@ -0,0 +1,11 @@
+// localStorage.setItem('nome' + 'mario')
+
+
+// let nome = localStorage.getItem('nome')
+// let idade = localStorage.getItem('idade')
+// let altura = localStorage.getItem('altura')
+
+// alert(nome + " - "+ idade + " - "+ altura)
+
+
+localStorage.clear()

b18570f29302691dfd570bbcc65c87229cef5923
FELIPE MENEGHEL <felipe_meneghel@estudante.sesisenai.org.br>
2024-08-19 14:25:31 -0300

    ramon aula

---
+++ e-commerce ramon/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const rotas = require("./rotas")
+
+
+app.use(bodyParser.json())
+
+app.use('/produtos', rotas)
+
+
+app.listen(3300)
---
+++ e-commerce ramon/rotas.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const controlador = require('./controlador')
+
+router.get('', )
+router.get('/:id',)
+router.post('', )
+router.post('/:id',)
+router.delete('/', )
+
+module.exports = router;

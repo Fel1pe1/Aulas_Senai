14d4bd02bbd7fd6a0fcc7006dd7989ff781e68f5
FELIPE MENEGHEL <felipe_meneghel@estudante.sesisenai.org.br>
2024-09-23 17:12:50 -0300

    teste

--- e-commerce_ramon/index.js
+++ e-commerce_ramon/index.js
@@ -3,17 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const rotas_produtos = require('./rotas/rotas_produtos');
 const rotas_clientes = require('./rotas/rotas_clientes');
-const rotas_autenticacao = require('./rotas/rotas_autenticacao')
-const cookiePaster = require('cookie-parser')
+const rotas_autenticacao = require('./rotas/rotas_autenticacao');
+const cookiePaster = require('cookie-parser');
 
-app.use(cookiePaster())
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
-// Middleware
+const swaggerDocument = YAML.load('./docs/documentacao.yaml')
+
+app.use(cookiePaster());
 app.use(bodyParser.json());
+
+
 // Rotas
 app.use('/produto', rotas_produtos);
 app.use('/clientes', rotas_clientes);
 app.use('/auth', rotas_autenticacao);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Porta
 app.listen(3300)
